4e90d3b2789539812362383a051f3b4998e5df46
zoniha <awaawapurin@gmail.com>
2022-07-11 00:05:14 +0900

    ✨ feat: Add article page

---
+++ routes/articles/[id].tsx
@@ -0,0 +1,60 @@
+/** @jsx h */
+import { h } from 'preact';
+import { Head } from '$fresh/src/runtime/head.ts';
+import { PageProps } from '$fresh/server.ts';
+import { Handlers } from '$fresh/server.ts';
+import { tw } from '@twind';
+import { Article, findArticleById } from '@db';
+import dayjs from 'dayjs/';
+import { marked } from 'marked';
+import sanitize from 'sanitize-html';
+
+interface Data {
+  article: Article;
+  parsedContent: string;
+}
+
+export const handler: Handlers<Data | null> = {
+  async GET(_, ctx) {
+    const { id } = ctx.params;
+    const article = await findArticleById(id);
+
+    if (!article) {
+      return ctx.render(null);
+    }
+
+    const parsed = marked(article.content);
+    const parsedContent = sanitize(parsed);
+
+    return ctx.render({ article, parsedContent });
+  },
+};
+
+export default function ArticlePage({ data }: PageProps<Data | null>) {
+  if (!data) {
+    return <div>Not Found</div>;
+  }
+
+  const { article, parsedContent } = data;
+
+  return (
+    <div class={tw('min-h-screen bg-gray-200')}>
+      <Head>
+        <title>{article.title}</title>
+      </Head>
+      <div class={tw('max-w-screen-sm mx-auto px-4 sm:px-6 md:px-8 pt-12 pb-20 flex flex-col')}>
+        <article class={tw('rounded-xl border p-5 shadow-md bg-white')}>
+          <header>
+            <h1 class={tw('font-extrabold text-5xl text-gray-800')}>{article.title}</h1>
+            <time class={tw('text-gray-500 text-sm')} dateTime={article.created_at}>
+              {dayjs(article.created_at).format('YYYY-MM-DD HH:mm:ss')}
+            </time>
+          </header>
+          <section class={tw('mt-6')}>
+            <p>{article.content}</p>
+          </section>
+        </article>
+      </div>
+    </div>
+  );
+}

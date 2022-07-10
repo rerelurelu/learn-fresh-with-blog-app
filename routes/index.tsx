f5ed873bdedfdb76df65d9bce089ff93f1ae9d11
zoniha <awaawapurin@gmail.com>
2022-07-10 12:34:53 +0900

    🎨 layout: Update

--- routes/index.tsx
+++ routes/index.tsx
@@ -4,6 +4,12 @@ import { PageProps } from '$fresh/server.ts';
 import { Head } from '$fresh/src/runtime/head.ts';
 import { tw } from '@twind';
 import { Handlers } from '$fresh/server.ts';
+import dayjs from 'https://esm.sh/dayjs@1.11.3/';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import 'dayjs/locale/ja';
+
+dayjs.extend(relativeTime);
+dayjs.locale('ja');
 
 interface Article {
   id: string;
@@ -31,20 +37,28 @@ export const handler: Handlers<Article[]> = {
 
 export default function Home({ data }: PageProps<Article[]>) {
   return (
-    <div>
+    <div class={tw('h-screen bg-gray-900')}>
       <Head>
         <title>Fresh Blog</title>
       </Head>
-      <div>
-        <h1 class={tw('text-teal-700')}>Fresh Blog</h1>
-        <section>
-          <h2>Posts</h2>
+      <div class={tw('max-w-screen-sm mx-auto px-4 sm:px-6 md:px-8 pt-12 pb-20 flex flex-col')}>
+        <h1 class={tw('text-gray-100 font-extrabold text-5xl')}>Fresh Blog</h1>
+        <section class={tw('mt-8')}>
+          <h2 class={tw('text-4xl font-bold text-gray-500 py-4')}>Posts</h2>
           <ul>
             {data.map((article) => (
-              <li key={article.id}>
+              <li class={tw('bg-gray-400 p-6 rounded-lg shadow-lg mb-4')} key={article.id}>
                 <a href={`articles/${article.id}`}>
-                  <h3>{article.title}</h3>
-                  <time dateTime={article.created_at}>{article.created_at}</time>
+                  <h3
+                    class={tw(
+                      'text-2xl font-bold mb-2 text-gray-600 hover:text-teal-600 hover:text-underline'
+                    )}
+                  >
+                    {article.title}
+                  </h3>
+                  <time class={tw('text-gray-500 text-sm')} dateTime={article.created_at}>
+                    {article.created_at}
+                  </time>
                 </a>
               </li>
             ))}

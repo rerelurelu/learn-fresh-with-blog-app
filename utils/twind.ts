3c983bfb2f55bbc0ee2bfddb5e157ab74ea2e890
zoniha <awaawapurin@gmail.com>
2022-07-10 12:14:37 +0900

    ✨ feat: Add some colors

--- utils/twind.ts
+++ utils/twind.ts
@@ -1,8 +1,20 @@
-import { IS_BROWSER } from "$fresh/runtime.ts";
-import { Configuration, setup } from "twind";
-export * from "twind";
+import { IS_BROWSER } from '$fresh/runtime.ts';
+import { Configuration, setup } from 'twind';
+import * as colors from 'twind/colors';
+
+export * from 'twind';
 export const config: Configuration = {
-  darkMode: "class",
-  mode: "silent",
+  theme: {
+    colors: {
+      gray: colors.gray,
+      teal: colors.teal,
+      cyan: colors.cyan,
+      sky: colors.sky,
+      emerald: colors.emerald,
+    },
+  },
+  darkMode: 'class',
+  mode: 'silent',
 };
+
 if (IS_BROWSER) setup(config);
